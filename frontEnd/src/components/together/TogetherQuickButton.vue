181a7d6b7e16b24956b992d069ae57fb137887b7
raccoon-coding <minsungt@naver.com>
2025-07-01 18:03:27 +0900

    [feat] Together Modal and Redirect Feature Develop

---
+++ frontEnd/src/components/together/TogetherQuickButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- 빠른방 만들기 -->
+    <div>
+        <button class="btn-create-room-quick">
+            <i class="fas fa-plus"></i>
+            빠른 방 만들기
+        </button>
+    </div>
+</template>
+
+<style scoped>
+@import url(../../assets/together/TogetherQuickButton.css);
+</style>
--- frontEnd/src/components/together/togetherCard.vue
+++ frontEnd/src/components/together/togetherCard.vue
@@ -2,12 +2,22 @@
 // defineProps({
 //     title: String,
 //     host: String,
-//     ....
+//     togetherUrl: String
 // })
+
+// 방 참가하기 버튼 클릭 시 실행될 함수
+const joinRoom = () => {
+  // props로 받은 URL로 이동
+  if (true) {
+    window.location.href = "together/12"
+  } else {
+    console.log('URL이 제공되지 않았습니다.')
+  }
+}
 </script>
 
 <template>
-  <div class="room-card">
+  <div class="room-card" @click="joinRoom">
     <div class="room-thumbnail">
       <img src="https://via.placeholder.com/320x180" alt="동영상 썸네일" />
       <div class="room-status">
@@ -38,7 +48,7 @@
           <img src="https://via.placeholder.com/30x30" alt="참가자" class="participant-avatar" />
         </div>
       </div>
-      <button class="btn-join-room" data-room-id="2">
+      <button class="btn-join-room" data-room-id="2"  @click="joinRoom">
         <i class="fas fa-sign-in-alt"></i>
         방 참가하기
       </button>
