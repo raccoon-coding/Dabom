c3f2fa5ce2449c87370ab805ee749c0d03a6d9db
tipsyboy <gudah13@naver.com>
2025-07-04 19:58:38 +0900

    style: design main index page ---
    feat: input main page router
    style: design sibebar component
    style: design main banner and search bar
    style: design video component

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import togetherRoutes from './togetherRouter.js' // 라우트 배열 import
 import authRoutes from './authRouter.js'
+import MainView from '@/views/MainView.vue'
 
 const routes = [
+  {
+    path: "/",
+    name: "main",
+    component: MainView
+  },
   ...authRoutes,
   ...togetherRoutes,
   // 다른 라우트들
