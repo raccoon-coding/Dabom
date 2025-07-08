52038e84ddc82d8712ed156d85b92c881a032305
flionme <sunginkim142@gmail.com>
2025-07-08 18:00:17 +0900

    api 연결 중...

---
+++ frontEnd/src/api/video_player/index.js
@@ -0,0 +1,36 @@
+import api from "@/plugins/axiosinterceptor";
+
+
+const getVideoInfo = async (req) => {
+    let data = {}
+    const requestUrl = `api/video-list2.json`
+
+    await api.get(requestUrl, req)
+        .then((res) => {
+            // console.log(response)
+            data = res.data
+        })
+        .catch((error) => {
+            data = error.data
+        })
+    return data
+};
+
+const getRecommendVideo = async (req) => {
+    let data = {};
+    let url = '/api/recommend-video.json';
+
+    await api.get(url, req)
+        .then((res) => {
+            data = res.data;
+        })
+        .catch((error) => {
+            data = error.data;
+        });
+
+    return data;
+}
+
+
+
+export default { getVideoInfo, getRecommendVideo }
