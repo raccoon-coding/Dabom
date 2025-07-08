1126ff5579d3a84e83211834064c9b6f9f46e112
raccoon-coding <minsungt@naver.com>
2025-07-08 12:04:25 +0900

    [feat] develop Channel Post Feature

--- frontEnd/src/views/channel/ChannelMainView.vue
+++ frontEnd/src/views/channel/ChannelMainView.vue
@@ -1,12 +1,21 @@
 <script setup>
 import channelnavigation from '@/components/channel/ChannelNavigation.vue';
+
 </script>
 
 <template>
 <channelnavigation />
-<RouterView />
+<div class="channel-content">
+    <router-view></router-view>
+</div>
+
 </template>
 
 <style scoped>
-
+/* Content Tabs */
+.channel-content {
+    max-width: 1400px;
+    margin: 0 auto;
+    padding: 2rem;
+}
 </style>
