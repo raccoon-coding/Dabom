6f44cca3e70b664f278cf8fc0418446337e78a3e
kbw <kbwook96@naver.com>
2025-07-08 09:05:05 +0900

    내채널, 채널 페이지 수정

---
+++ frontEnd/src/router/channelRouter.js
@@ -0,0 +1,12 @@
+
+import channel from "@/views/channel.vue"
+
+const channelRouter = [
+    {
+        path: '/channel',
+        name: 'channel',
+        component: channel,
+    }
+]
+
+export default channelRouter
