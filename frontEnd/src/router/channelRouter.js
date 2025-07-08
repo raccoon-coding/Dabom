da4f1ecb4fbc9a617de7e9b20a15e837f0e0a258
raccoon-coding <minsungt@naver.com>
2025-07-08 12:43:10 +0900

    [Refac] refactoring Channel

--- frontEnd/src/router/channelRouter.js
+++ frontEnd/src/router/channelRouter.js
@@ -1,12 +1,28 @@
-
-import channel from "@/views/channel.vue"
+import community from "@/views/channel/subview/Community.vue"
+import playlist from "@/views/channel/subview/playlist/Playlist.vue"
+import channel from "@/views/channel/ChannelMainView.vue"
+import about from "@/views/channel/subview/About.vue"
 
 const channelRouter = [
     {
         path: '/channel',
         name: 'channel',
         component: channel,
-    }
+        children: [
+            {
+                path:'/channel/playlists',
+                component: playlist,
+            },
+            {
+                path:'/channel/community',
+                component: community,
+            },
+            {
+                path:'/channel/about',
+                component: about,
+            },
+        ],
+    },
 ]
 
 export default channelRouter
---
+++ frontEnd/src/views/channel/subview/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+import channelAboutInfo from '@/components/channel/ChannelAboutInfo.vue';
+</script>
+
+<template>
+<channelAboutInfo />
+</template>
+
+<style scoped>
+
+</style>
