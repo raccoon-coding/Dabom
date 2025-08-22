667fe562dbeb435ef32bbe0bc1d2e578755eeb1f
raccoon-coding <minsungt@naver.com>
2025-08-22 17:56:46 +0900

    [DEVELOP] together api 뚫기

--- frontEnd/src/api/together/index.js
+++ frontEnd/src/api/together/index.js
@@ -2,7 +2,7 @@ import api from "@/plugins/axiosinterceptor";
 
 
 export const getRandomTogetherList = async () => {
-    const requestUrl = `/api/random-together.json`
+    const requestUrl = `/api/together`
     let data = {}
 
     await api.get(requestUrl)
@@ -16,12 +16,27 @@ export const getRandomTogetherList = async () => {
     return data
 };
 
-export const getTogetherSearch = async () => {
-    const requestUrl = `/api/search-together.json`
+export const getTogetherListInMember = async () => {
+  const requestUrl = `/api/together/member`
+  let data = {}
+
+  await api.get(requestUrl)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export const searchTogether = async (body) => {
+    const requestUrl = `/api/together/search`
     let data = {}
 
     // await api.get(requestUrl, {params: searc h})
-    await api.get(requestUrl)
+    await api.post(requestUrl, body)
         .then((response) => {
             console.log(response)
             data = response.data
@@ -32,4 +47,80 @@ export const getTogetherSearch = async () => {
     return data
 };
 
-export default { getRandomTogetherList, getTogetherSearch }
+export const saveTogether = async (body) => {
+  const requestUrl = `/api/together/save`
+  let data = {}
+
+  await api.post(requestUrl, body)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export const joinTogether = async (togetherIdx) => {
+  const requestUrl = `/api/together/` + togetherIdx
+  let data = {}
+
+  await api.get(requestUrl)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export const joinOpenTogether = async (togetherIdx) => {
+  const requestUrl = `/api/together/` + togetherIdx
+  let data = {}
+
+  await api.post(requestUrl)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export const joinTogetherWithCode = async (togetherIdx, code) => {
+  const requestUrl = `/api/together/` + togetherIdx + `/code`
+  let data = {}
+
+  await api.post(requestUrl, code)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export const leaveTogether = async (togetherIdx) => {
+  const requestUrl = `/api/together/` + togetherIdx + `/member`
+  let data = {}
+
+  await api.delete(requestUrl)
+    .then((response) => {
+      console.log(response)
+      data = response.data
+    })
+    .catch((error) => {
+      data = error.data
+    })
+  return data
+};
+
+export default { getRandomTogetherList, getTogetherListInMember, searchTogether,
+  joinTogether, joinOpenTogether, joinTogetherWithCode, saveTogether, leaveTogether }
