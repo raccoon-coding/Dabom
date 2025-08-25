c51f63387e085cba566f778cac5174f32c419900
raccoon-coding <minsungt@naver.com>
2025-08-25 17:08:09 +0900

    [DEVELOP] together 채팅까지 구현 완료

--- frontEnd/src/views/TogetherRoom.vue
+++ frontEnd/src/views/TogetherRoom.vue
@@ -1,48 +1,135 @@
 <script setup>
-import TogetherMasterModal from '@/components/together_room/TogetherMasterModal.vue';
-import TogetherRoomChat from '@/components/together_room/TogetherRoomChat.vue';
-import TogetherRoomNavigator from '@/components/together_room/TogetherRoomNavigator.vue';
-import Video_Player_Component from '@/components/Video_Player/Video_Player_Component.vue';
+import TogetherMasterModal from '@/components/together_room/TogetherMasterModal.vue'
+import TogetherRoomChat from '@/components/together_room/TogetherRoomChat.vue'
+import TogetherRoomNavigator from '@/components/together_room/TogetherRoomNavigator.vue'
+import Video_Player_Component from '@/components/Video_Player/Video_Player_Component.vue'
 
-import {onMounted, reactive, ref} from 'vue'
-import {useRoute} from "vue-router";
+import Stomp from 'stompjs'
+import { onMounted, reactive, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import api from '@/api/together/'
 
+const socket = ref(null)
+const message = reactive({
+  messages: [],
+})
+const route = useRoute()
+const togetherInfo = reactive({
+  togetherIdx: '',
+  title: '',
+  maxMemberNum: '',
+  joinMemberNumber: '',
+  master: '',
+  isOpen: '',
+})
 const stateModal = reactive({
-    chatModal: false,
-    masterModal: false,
-});
-const testUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+  chatModal: false,
+  masterModal: false,
+})
+const subscribed = ref(false)
+const togetherIdx = ref('')
+const isMaster = ref(false)
+const testUrl = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+
+// api로 Master 확인 후, 데이터 내려주기
+const getTogetherInfo = async () => {
+  let res = await api.getTogetherInfo(togetherIdx.value)
+  if (res.code === 200) {
+    loadTogetherInfo(res.data)
+  }
+}
+
+const getIsMaster = async () => {
+  let res = await api.isMaster(togetherIdx.value)
+  if (res.code === 200 && res.data.isMaster === true) {
+    isMaster.value = true
+  }
+}
+
+const loadTogetherInfo = (data) => {
+  togetherInfo.togetherIdx = data.togetherIdx
+  togetherInfo.isOpen = data.isOpen
+  togetherInfo.title = data.title
+  togetherInfo.master = data.master
+  togetherInfo.maxMemberNum = data.maxMemberNum
+  togetherInfo.joinMemberNumber = data.joinMemberNumber
+}
+
+const connectWebSocket = () => {
+  const ws = new WebSocket('ws://localhost:8080/chat')
+  const client = Stomp.over(ws)
+  socket.value = client
+  client.connect(
+    {},
+    (frame) => {
+      console.log('연결 됐을 때 ')
+      socket.value.subscribe(`/topic/together/${togetherIdx.value}`, (msg) => {
+        try {
+          const data = JSON.parse(msg.body)
+          console.log(data)
+          message.messages.push(data)
+        } catch(e) {
+          console.error("메시지 파싱 실패", e)
+        }
+      })
+      subscribed.value = true
+    },
+    (err) => {
+      console.error('연결 실패', err)
+    },
+  )
+}
 
 const openChatModal = () => {
-    stateModal.chatModal = true;
+  stateModal.chatModal = true
 }
 
 const closeChatModal = () => {
-    stateModal.chatModal = false;
+  stateModal.chatModal = false
 }
 
 const openMasterModal = () => {
-    stateModal.masterModal = true;
+  stateModal.masterModal = true
 }
 
 const closeMasterModal = () => {
-    stateModal.masterModal = false;
+  stateModal.masterModal = false
 }
+onMounted(() => {
+  togetherIdx.value = route.params.id
+  getTogetherInfo()
+  getIsMaster()
+  connectWebSocket()
+})
 </script>
 
 <template>
-    <TogetherRoomNavigator @open_chat_modal="openChatModal()" @open_master_modal="openMasterModal()" />
-    <!-- Main Content -->
-        <div class="video-section" id="videoSection">
-            <!-- Video Container -->
-            <div class="video-container" :class="{ 'modal-open': stateModal.chatModal }" >
-                <div class="video-player">
-                    <Video_Player_Component :video_url="testUrl" :is_open_modal="stateModal.chatModal" />
-                </div>
-            </div>
-        </div>
-        <TogetherMasterModal v-if="stateModal.masterModal" @close_modal="closeMasterModal()"></TogetherMasterModal>
-        <TogetherRoomChat v-if="stateModal.chatModal" @close_modal="closeChatModal()" @open_master_modal="openMasterModal"></TogetherRoomChat>
+  <TogetherRoomNavigator
+    :isMaster="isMaster"
+    @open_chat_modal="openChatModal()"
+    @open_master_modal="openMasterModal()"
+  />
+  <!-- Main Content -->
+  <div class="video-section" id="videoSection">
+    <!-- Video Container -->
+    <div class="video-container" :class="{ 'modal-open': stateModal.chatModal }">
+      <div class="video-player">
+        <Video_Player_Component :video_url="testUrl" :is_open_modal="stateModal.chatModal" />
+      </div>
+    </div>
+  </div>
+  <TogetherMasterModal
+    v-if="stateModal.masterModal"
+    @close_modal="closeMasterModal()"
+  ></TogetherMasterModal>
+  <TogetherRoomChat
+    v-if="stateModal.chatModal && subscribed"
+    :isMaster="isMaster"
+    :socket="socket"
+    :messages="message.messages"
+    @close_modal="closeChatModal()"
+    @open_master_modal="openMasterModal"
+  />
 </template>
 
 <style scoped>
