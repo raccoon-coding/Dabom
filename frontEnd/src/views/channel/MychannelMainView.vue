69811d874fa27ffe4bf82a7b0bd086d1efff60f6
kbw <kbwook96@naver.com>
2025-09-01 17:24:57 +0900

    충돌후

--- frontEnd/src/views/channel/MychannelMainView.vue
+++ frontEnd/src/views/channel/MychannelMainView.vue
@@ -1,30 +1,30 @@
-<!--<script setup>-->
-<!--import { ref } from 'vue'-->
-<!--import VideoManagementSection from '@/components/channel/video-management/VideoManagementSection.vue';-->
-<!--import PlaylistSection from '@/components/channel/playlist-management/PlaylistSection.vue';-->
-<!--import CommunitySection from '@/components/channel/community-management/CommunitySection.vue';-->
-<!--import InfoSection from '@/components/channel/information-management/InfoSection.vue';-->
+<script setup>
+import { ref } from 'vue'
+import VideoManagementSection from '@/components/channel/video-management/VideoManagementSection.vue';
+import PlaylistSection from '@/components/channel/playlist-management/PlaylistSection.vue';
+import CommunitySection from '@/components/channel/community-management/CommunitySection.vue';
+import InfoSection from '@/components/channel/information-management/InfoSection.vue';
 
-<!--// 현재 활성 섹션 상태-->
-<!--const currentSection = ref('videos')-->
+// 현재 활성 섹션 상태-->
+const currentSection = ref('videos')
 
-<!--const handleNavigation = (section) => {-->
-<!--  currentSection.value = section-->
-<!--}-->
+const handleNavigation = (section) => {
+ currentSection.value = section
+}
 
-<!--</script>-->
+</script>
 
-<!--<template>-->
-<!--  <div class="channel-dashboard">-->
-<!--    <main class="dashboard-main">-->
-<!--      <PlaylistSection :isActive="currentSection === 'playlists'"/>-->
-<!--      <CommunitySection :isActive="currentSection === 'community'"/>-->
-<!--      <InfoSection :isActive="currentSection === 'info'"/>-->
-<!--      <VideoManagementSection :isActive="currentSection === 'manage'"/>-->
-<!--    </main>-->
-<!--  </div>-->
-<!--</template>-->
+<template>
+ <div class="channel-dashboard">
+   <main class="dashboard-main">
+     <PlaylistSection :isActive="currentSection === 'playlists'"/>
+<CommunitySection :isActive="currentSection === 'community'"/>
+    <InfoSection :isActive="currentSection === 'info'"/>
+      <VideoManagementSection :isActive="currentSection === 'manage'"/>
+    </main>
+  </div>
+</template>
 
-<!--<style scoped>-->
-<!--@import url(@/assets/channel/mychannel.css);-->
-<!--</style>-->
+<style scoped>
+@import url(@/assets/channel/mychannel.css);
+</style>
